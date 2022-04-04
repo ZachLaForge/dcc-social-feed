53b7f3fa02e0ac3dd5227e7d03267805f4df877c
Zachary LaForge <zach.laforge@hotmail.com>
2022-04-04 17:52:16 -0400

    added entry and display files + starter functionality

---
+++ src/components/displayEntry/DisplayEntry.jsx
@@ -0,0 +1,17 @@
+
+const DisplayEntry = (props) => {
+    return ( 
+        <tbody>
+            {props.parentEntries.map((entry, index) => {
+                return(
+                    <tr key={index}>
+                        <td>{entry.name}</td>
+                        <td>{entry.post}</td>
+                    </tr>
+                )
+            })}
+        </tbody>
+     );
+}
+ 
+export default DisplayEntry;
